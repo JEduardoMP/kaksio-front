470d1fd5bb9304427dfe3c97ffef0936bc41cb7a
Jose Eduardo <eduardomarque2009@gmail.com>
2022-03-03 13:15:39 -0600

    Styled all the views and components as successfully

--- src/views/specificPage/specificPage.jsx
+++ src/views/specificPage/specificPage.jsx
@@ -8,14 +8,17 @@ const SpecificPage = () => {
   console.log(location.state);
   return (
     <div className="main-container">
-      <div className="single-img-container">
+      <div className="single-cat-container">
         <h2>{location.state.name}</h2>
+        <div className="single-img-container">
+          <img
+            style={{ maxWidth: "100%" }}
+            src={`data:image/png;base64,${location.state.image}`}
+            alt={location.state.name}
+          />
+        </div>
+        <p>{location.state.text}</p>
       </div>
-      <img
-        src={`data:image/png;base64,${location.state.image}`}
-        alt={location.state.name}
-      />
-      <p>{location.state.text}</p>
     </div>
   );
 };
